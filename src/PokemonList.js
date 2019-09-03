80fa741bff0ef89d690e59825353f2e4dd0ecae5
Jeroen Haisma <J_Haisma@hotmail.com>
2019-09-03 20:16:48 -0300

    first commit

---
+++ src/PokemonList.js
@@ -0,0 +1,15 @@
+// src/PokemonList.js
+// rfc
+
+import React from 'react'
+
+export default function PokemonList({ pokemon }) {
+
+  return (
+    <div>
+      {pokemon.map( p => (
+          <div key={p}>{p}</div>
+        ))}
+    </div>
+  )
+}
